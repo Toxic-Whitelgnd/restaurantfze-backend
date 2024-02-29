3ef7f6a7913acf03cbf4fbb08c3a439ef7914146
unknown <sanvedak90670@gmail.com>
2024-03-01 01:27:24 +0530

    css update

--- frontend/src/componets/Pages/Pages.jsx
+++ frontend/src/componets/Pages/Pages.jsx
@@ -6,33 +6,21 @@ const Pages = () => {
     return (
         <div class='admin-panel-body'>
             <div class='admin-panel-head'>
-            <h1>Admin Panel</h1>
+                <h1>Admin Panel</h1>
             </div>
             <div class='option-panel'>
-            <div class='option-panel-1'>
-                <NavLink to='/admin/homepanel' className='HOME-P'style={{ textDecoration: 'none'}}>Home Panel </NavLink>
-                <br></br>
-                <NavLink to={'/admin/tablepanel'} className='TABLE-P'style={{ textDecoration: 'none'}}>Table Panel</NavLink>
-                <br></br>
-                <NavLink to={'/admin/foodpanel'} className='FOOD-P'style={{ textDecoration: 'none'}}>Food Panel </NavLink>
-                <br></br>
-                
-            </div>
-            <div  class='option-panel-2'>
-                <NavLink to={'/admin/kitchenpanel'}className='KITCHEN-P'style={{ textDecoration: 'none'}}>Kitchen Panel</NavLink>
-                <br></br>
-                <NavLink to={'/admin/billpanel'}className='BILL-P'style={{ textDecoration: 'none'}}>Bill Panel</NavLink>
-                <br></br>
-                <NavLink to={'/admin/waiterpanel'}>Waiter Panel</NavLink>
-                <br></br>
-                
-            </div>
-            <div class="option-panel-3"> 
-                 
-                <NavLink to={'/admin/deliverypanel'}className='DELIVERY-P'style={{ textDecoration: 'none'}}>DeliveryOrder<br></br> Panel</NavLink>
-                <br></br>
-                <NavLink to={'/admin/messpanel'}className='messpanel'style={{ textDecoration: 'none'}}>Mess Panel</NavLink>
-            </div>
+                <div class='option-panel-row'>
+                    <NavLink to='/admin/homepanel' className='HOME-P' style={{ textDecoration: 'none' }}>Home Panel </NavLink>
+                    <NavLink to={'/admin/tablepanel'} className='TABLE-P' style={{ textDecoration: 'none' }}>Table Panel</NavLink>
+                    <NavLink to={'/admin/foodpanel'} className='FOOD-P' style={{ textDecoration: 'none' }}>Food Panel </NavLink>
+                    <NavLink to={'/admin/kitchenpanel'} className='KITCHEN-P' style={{ textDecoration: 'none' }}>Kitchen Panel</NavLink>
+                </div>
+                <div class='option-panel-row'>
+                    <NavLink to={'/admin/billpanel'} className='BILL-P' style={{ textDecoration: 'none' }}>Bill Panel</NavLink>
+                    <NavLink to={'/admin/waiterpanel'} className='WAITER-P' style={{ textDecoration: 'none' }}>Waiter Panel</NavLink>
+                    <NavLink to={'/admin/deliverypanel'} className='DELIVERY-P' style={{ textDecoration: 'none' }}>Delivery Order Panel</NavLink>
+                    <NavLink to={'/admin/messpanel'} className='messpanel' style={{ textDecoration: 'none' }}>Mess Panel</NavLink>
+                </div>
             </div>
         </div>
     );
